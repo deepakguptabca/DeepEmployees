ea9863a6de594e1beb169f31c00e8cfe81a76447
deepakguptabca <deepakguptamarketer@gmail.com>
2026-05-01 21:46:20 +0530

    added login signup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,14 +1,17 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 
 export const signup = async (req,res) =>{
     const {fullName,email,password} = req.body;
     try {
+        if(!email || !fullName || !password) return res.status(400).json({message:"fill your all details"})
+
         if(password.length<6){
             return res.status(400).json({message:"password must be atleast 6 characters"})
         }
 
-        const user = await User.findOne(email);
+        const user = await User.findOne({email});
 
         if(user) return res.status(400).json({message: "user already exists"})
 
@@ -23,24 +26,60 @@ export const signup = async (req,res) =>{
 
         if(newUser) {
             //generate jwt token here
-            
+            generateToken(newUser._id,res);
+            await newUser.save();
+            res.status(201).json({message:"new user is succesfully created",
+                user_id: newUser._id,
+                fullName:newUser.fullName,
+                email:newUser.email,
+                
+            })
 
         }else{
             res.status(400).json({message:"invalid user data"});
         }
 
     } catch (error) {
-        
+        console.log("Error in signup controller",error.message)
+        res.status(400).json({message:"interval server error"})
     }
 
 }
 
-export const login = (req,res) =>{
+export const login = async (req,res) =>{
+     const {email,password} = req.body;
+    try {
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(404).json({message:"user not found"})
+        }
 
+        const isPasswordCorrect = await bcrypt.compare(password,user.password)
+        if(!isPasswordCorrect){
+           return res.status(400).json({message:"your password is wrong"})
+        }
+
+        generateToken(user._id,res)
+
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            email:user.email
+        })
+
+    } catch (error) {
+        console.log("error in login crendtials",error.message)
+        res.status(500).json({message: "Interval server error"})
+    }
 
 }
 
 export const logout = (req,res) =>{
-
+    try {
+        res.cookie("jwt","",{maxAge:0})
+        res.status(200).json({message:"loggout successfully"})
+    } catch (error) {
+        console.log("error in logout controller",error.message)
+    }
 
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
@@ -15,6 +16,7 @@ app.get('/', (req, res) => {
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
 
 // for authencation
 app.use("/api/auth",authRoutes)
