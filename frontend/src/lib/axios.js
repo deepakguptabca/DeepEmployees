b7c5bf6acfc1184108e6d14650e75e8229c5817e
= <deepakguptamarketer@gmail.com>
2026-07-01 18:40:54 +0530

    changes for production deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_KEY = import.meta.env.VITE_API_URL;
+
 const axiosInstance = axios.create({
-    baseURL : "http://localhost:5001/api",
+    baseURL : API_KEY,
     withCredentials : true,
 })
 
